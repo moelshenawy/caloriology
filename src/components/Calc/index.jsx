6d51812221ce619f532d6072a67727e1bfe67c56
moelshenawy <mohamedgk5@gmail.com>
2023-02-18 01:04:37 +0200

    add api calories calc

--- src/components/Calc/index.jsx
+++ src/components/Calc/index.jsx
@@ -6,26 +6,35 @@ import { CheckedRadio, CheckRadio } from '../../assets/svgs'
 import { motion } from 'framer-motion';
 import { GrClose } from 'react-icons/gr'
 import { useTranslation } from 'react-i18next';
-
+import axios from 'axios'
+import { useEffect } from 'react';
+import Joi from "joi";
+import { use } from 'i18next';
 
 const Calc = () => {
   const [showCalc, setShowCalc] = useState(false)
+
   const [male, setMale] = useState(false)
   const [female, setFemale] = useState(false)
+  const [errorList, setErrorList] = useState()
+  const [isLoading, setIsLoading] = useState()
+  const [gender, setGender] = useState('')
   const { t, i18n } = useTranslation();
-
   const [calories, setCalories] = useState({
     age: "",
     height: "",
     weight: "",
+    userGender: "",
+    activity: ''
   });
-
 
   const changeFemale = () => {
     if (male === true) {
       setMale(false)
     }
     setFemale((prev) => !prev)
+    setGender('')
+
 
   }
 
@@ -34,22 +43,99 @@ const Calc = () => {
       setFemale(false)
     }
     setMale((prev) => !prev)
-
+    setGender('')
   }
+
+
+
+  useEffect(() => {
+    if (female === true) {
+      setGender('female')
+    }
+
+    if (male === true) {
+      setGender('male')
+    }
+
+  }, [male, female])
+
+  useEffect(() => {
+    changeGender()
+
+  }, [gender])
+
 
 
   const getCalories = (e) => {
     let userCalories = { ...calories };
-    userCalories[e.target.name] = e.target.value;
+    userCalories[e.target.name] = e.target.value
     setCalories(userCalories);
+
   };
 
+  const changeGender = () => {
+    const newGender = { ...calories };
+    newGender.userGender = gender
+    setCalories(newGender)
 
+  }
 
 
   const submitForm = async (e) => {
     e.preventDefault();
     // Logic
+
+    let validationRes = validationForm();
+
+    if (validationRes.error) {
+      // push error to error List Array
+      setErrorList(validationRes.error.details)
+      setIsLoading(false);
+
+    } else {
+      setErrorList([])
+
+
+      const data = await axios.get(`https://fitness-calculator.p.rapidapi.com/dailycalorie`,
+
+        {
+          params: {
+            age: calories.age,
+            gender: calories.userGender,
+            height: calories.height,
+            weight: calories.weight,
+            activitylevel: calories.activity
+          },
+          headers: {
+            "X-RapidAPI-Key": "cf296e0620mshd5824004b4fcfd7p191180jsn58053e2b12fd",
+            "X-RapidAPI-Host": 'fitness-calculator.p.rapidapi.com'
+          }
+
+        }
+
+
+      )
+
+      console.log(data)
+    }
+
+
+  };
+
+
+
+  console.log(errorList)
+  const validationForm = () => {
+    let scheme = Joi.object({
+      age: Joi.number().min(5).max(80).required(),
+      weight: Joi.number().min(40).max(160).required(),
+      height: Joi.number().min(130).max(230).required(),
+      userGender: Joi.string().required(),
+      activity: Joi.items(["Sedentary: little or no exercise", "Exercise 1-3 times/week", "Exercise 4-5 times/week", "Daily exercise or intense exercise 3-4 times/week"]).required(),
+
+    });
+
+    return scheme.validate(calories, { abortEarly: false });
   };
 
 
@@ -58,7 +144,7 @@ const Calc = () => {
       <section id='calc' className={`${showCalc && 'pb-5'}`}>
         <div className="container">
           <div className="calc-section "
-            dir={`${i18n.language ==="ar" ? "rtl" : "ltr"}`}>
+            dir={`${i18n.language === "ar" ? "rtl" : "ltr"}`}>
             <Link to='/food-calc' className="calc-svg">
               <Calculator />
             </Link>
@@ -82,12 +168,12 @@ const Calc = () => {
                 <GrClose />
               </div>
 
-              <form action="" onClick={submitForm}>
-                
+              <form action="" onSubmit={submitForm}>
+
                 <div className="age">
                   <label htmlFor="age">Age</label>
                   <input name='age' onChange={getCalories}
-                    type="text" placeholder='25' />
+                    type="number" placeholder='25' />
                   <span>cm</span>
                 </div>
 
@@ -104,20 +190,36 @@ const Calc = () => {
 
                   </div>
 
+
+
+
                 </div>
 
                 <div className="height">
                   <label htmlFor="height">Height</label>
                   <input name='height' onChange={getCalories}
-                    type="text" placeholder='180' />
+                    type="number" placeholder='180' />
                   <span>cm</span>
                 </div>
 
                 <div className="weight">
                   <label htmlFor="weight">Weight</label>
                   <input name='weight' onChange={getCalories}
-                    type="text" placeholder='180' />
+                    type="number" placeholder='180' />
                   <span>cm</span>
+                </div>
+
+                <div className="activity" onChange={getCalories}>
+                  <label htmlFor="activity">Activity</label>
+                  <select name="activity" id="">
+                    <option value="none">Select your weekly activity</option>
+                    <option value="level_1">Sedentary: little or no exercise</option>
+                    <option value="level_2">Exercise 1-3 times/week</option>
+                    <option value="level_3">Exercise 4-5 times/week</option>
+                    <option value="level_4">Daily exercise or intense exercise 3-4 times/week</option>
+                    <option value="level_5">Intense exercise 6-7 times/week</option>
+                    <option value="level_6">Very intense exercise daily, or physical job</option>
+                  </select>
                 </div>
 
                 <div className="btns-container">
